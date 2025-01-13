54cad9603014783a787e4a316ab25816cf3bba38
Nathan <nathan@Nathans-MBP.lan>
2025-01-13 16:41:03 -0500

    include being able to run mistral0.3

--- llms/utils.py
+++ llms/utils.py
@@ -14,7 +14,8 @@ def get_device() -> torch.device:
     if torch.cuda.is_available():
         return torch.device("cuda")
     elif torch.backends.mps.is_available():
-        return torch.device("mps")
+#         return torch.device("mps")
+        return torch.device("cpu")
     else:
         return torch.device("cpu")
     
